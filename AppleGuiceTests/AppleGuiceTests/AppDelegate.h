252a8b7eb58aaab31824272bc05ca64fa3d7e045
Tomer Shiri <tomer@chegg.com>
2013-03-23 02:53:51 +0200

    Extracted test target to a new project. Shold run from command line and teamcity

---
+++ AppleGuiceTests/AppleGuiceTests/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AppleGuiceTests
+//
+//  Created by Tomer Shiri on 3/23/13.
+//  Copyright (c) 2013 Tomer Shiri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AppleGuiceTests/AppleGuiceTests/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AppleGuiceTests
+//
+//  Created by Tomer Shiri on 3/23/13.
+//  Copyright (c) 2013 Tomer Shiri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
